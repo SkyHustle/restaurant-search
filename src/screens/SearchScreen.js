25b029d68f8e1184be9fadfad5d07bffa99b8fe9
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-14 14:45:54 +1100

    grab a default amount of restaurants once the user lands on the page

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log("this the term yall: ", searchTerm)
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'san jose'
         }
       });
@@ -23,12 +24,16 @@ const SearchScreen = () => {
     }
   };
 
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return(
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       <Text>Results: {results.length}</Text>
       { errorMessage ? <Text>{errorMessage}</Text> : null }
