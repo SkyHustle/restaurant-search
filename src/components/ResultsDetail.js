28747493907a039f3b46148a1f9d3d32a74b9c7e
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-15 10:18:49 +1100

    display and style image for each restaurant

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,21 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 const ResultsDetail = ({ result }) => {
   return(
     <View>
+      <Image
+        style={styles.image}
+        source={{ uri: result.image_url }}
+      />
       <Text>Name: {result.name} - Price {result.price}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  }
+});
 
 export default ResultsDetail;
