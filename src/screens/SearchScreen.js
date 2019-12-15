7a75bd9a19cbaa0b24d5e0fd573dc43600f2aace
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-15 11:03:04 +1100

    add vertical scrolling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -15,7 +15,7 @@ const SearchScreen = () => {
   };
 
   return(
-    <View>
+    <View style={{ flex: 1 }}>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
@@ -23,10 +23,11 @@ const SearchScreen = () => {
       />
 
       { errorMessage ? <Text>{errorMessage}</Text> : null }
-
-      <ResultsList title='Cost Effective' results={filterResultsByPrice('$')} />
-      <ResultsList title='Bit Pricier' results={filterResultsByPrice('$$')} />
-      <ResultsList title='Big Spender' results={filterResultsByPrice('$$$')} />
+      <ScrollView>
+        <ResultsList title='Cost Effective' results={filterResultsByPrice('$')} />
+        <ResultsList title='Bit Pricier' results={filterResultsByPrice('$$')} />
+        <ResultsList title='Big Spender' results={filterResultsByPrice('$$$')} />
+      </ScrollView>
     </View>
   );
 };
