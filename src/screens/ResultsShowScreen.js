63eb0034d17a14328cc6c29a9b3d823b09ba3857
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-16 12:35:29 +1100

    display images of restaurant on the restaurants show page

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    // this code will only run 1 time
+    getResult(id);
+  }, []);
+
+  if(!result) {
+    return null;
+  };
 
   return(
     <View>
-      <Text>Results Show Screen {id}</Text>
+      <FlatList
+        horizontal={false}
+        showsHorizontalScrollIndicator={false}
+        data={result.photos}
+        keyExtractor={(photo) => photo }
+        renderItem={( {item} ) => {
+          return (
+            <Image
+              style={styles.image}
+              source={{ uri: item }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginBottom: 5
+  }
+});
 
 export default ResultsShowScreen;
